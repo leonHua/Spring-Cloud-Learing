f01ebfcb15412aa6ed4e01106e34927eefe8bfff
open_leon <open_leon@163.com>
2019-05-30 10:06:23 +0800

    update prometheus

--- Prometheus/leon-consumer/src/main/java/cn/leon/leonconsumer/LeonConsumerApplication.java
+++ Prometheus/leon-consumer/src/main/java/cn/leon/leonconsumer/LeonConsumerApplication.java
@@ -1,9 +1,13 @@
 package cn.leon.leonconsumer;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,5 +27,10 @@ public class LeonConsumerApplication {
 			http.authorizeRequests().anyRequest().permitAll()
 					.and().csrf().disable();
 		}
+	}
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 }
--- Prometheus/leon-provider/src/main/java/cn/leon/leonprovider/LeonProviderApplication.java
+++ Prometheus/leon-provider/src/main/java/cn/leon/leonprovider/LeonProviderApplication.java
@@ -1,8 +1,12 @@
 package cn.leon.leonprovider;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,5 +25,10 @@ public class LeonProviderApplication {
 			http.authorizeRequests().anyRequest().permitAll()
 					.and().csrf().disable();
 		}
+	}
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 }
